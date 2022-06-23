e2c0292a50d68f456340f2b0b16f462dc7937e69
Aswathy Rajeev <aswathy.rajeev@ucdconnect.ie>
2022-06-23 12:27:01 +0100

    Adding Material UI into React Framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,9 +17,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
+      
       </header>
+
     </div>
   );
 }
